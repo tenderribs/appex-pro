a6d25ab80444f81ec28e783c25440067441299bf
tenderribs <dc3dude@gmail.com>
2018-06-03 09:42:24 +0200

    kdldsfj

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+// the username.DbName and password are defined in env.config.php file my name jeff
+require_once('env.config.php');
+    try {
+        $pdo = new PDO("mysql:host=$servername;dbname=$DbName", $username, $password);
+        // set the PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected successfully"; 
+        }
+    catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage();
+        }
+?>
---
+++ config/env.config.php
@@ -0,0 +1,6 @@
+<?php
+    $DbName = "appex_home";
+    $username = "appex_home";
+    $password = "TeYqM8WgNKgHwGQ9";
+    $servername = "localhost";   
+?>
